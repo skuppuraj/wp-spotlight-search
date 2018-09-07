8425333c932dd8e99278cca16310d47739b3e3a1
rajkuppu <kuppuraj@revmakx.com>
2018-09-08 00:14:39 +0530

    removed unwanted code

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,20 +1,4 @@
 jQuery(document).ready(function() {
-    //   jQuery('.ui.search').search({
-    //     apiSettings: {
-    //       url: my_ajax_object.ajax_url+'?q={query}',
-    //       'type': 'post',
-    //       data : {
-    //         action : 'post_love_add_love',
-    //       }
-    //     },
-    //     fields: {
-    //       results : 'items',
-    //       title   : 'name',
-    //       url     : 'html_url'
-    //     },
-    //     minCharacters : 3
-    // });
-
     jQuery('.ui.search')
       .search({
         source: wp_spotlite_full_menu,
@@ -26,11 +10,6 @@ jQuery(document).ready(function() {
               'category'
             ],
         maxResults : 10,
-
-        // fields: {
-        //           title   : 'name',
-        //           url     : 'html_url',
-        //         },
       })
     ;
 });
