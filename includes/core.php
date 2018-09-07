a673b5a51af830614f45de6ccf162cd307b355a3
rajkuppu <kuppuraj@revmakx.com>
2018-09-07 23:12:00 +0530

    Functions seperated

--- includes/core.php
+++ includes/core.php
@@ -54,9 +54,58 @@ class WP_Spotlite_Core{
 
 	public static function get_search_content(){
 		global $submenu, $menu, $wp_admin_bar, $wpdb;
-		$post_types = get_post_types('', 'object');
+		$final_response = array();
+		$all_post_types = WP_Spotlite_Core::get_all_searchable_post();
+		$final_response = array_merge($final_response,$all_post_types);
+		$searchabel_menu = WP_Spotlite_Core::get_searchable_menu();
+		$join = array_merge($final_response,$searchabel_menu);
+
+		return $join;
+	}
+	public static function get_searchable_menu(){
+		global $submenu, $menu;
+		$full_array = array();
+		$join = array();
+		foreach ($menu as $key => $value) {
+		    $home_url_part = pathinfo( $value[2]);
+		    if (!empty($submenu[$value[2]])) {
+		        foreach ($submenu[$value[2]] as $k => $v) {
+		            $temp = array();
+		            $temp['title']= $v[0];
+		            $temp['url']= $v[2];
+		            $temp['parent_url'] = $value[2];
+		            $temp['category'] = $value[0];
+		            $html_url_parts = pathinfo( $temp['url']);
+		            $parent_url_parts = pathinfo( $temp['parent_url']);
+		            if ((empty($html_url_parts['extension']) && empty($parent_url_parts['extension']))) {
+		                $temp['url'] = 'admin.php?page='. $temp['url'];
+		            }elseif (empty($html_url_parts['extension']) && ( !empty($parent_url_parts['extension']) && $parent_url_parts['extension'] == 'php')){
+		                $temp['url'] = $temp['parent_url'].'?page='.$temp['url'];
+		            }elseif (empty($html_url_parts['extension']) && ( !empty($parent_url_parts['extension']))) {
+		                $temp['url'] = $temp['parent_url'].'&'.$temp['url'];
+		            }
+		            array_push($full_array, $temp);
+		        }
+		    }elseif(!empty($value[0])){
+		        $temp = array();
+		        $temp['title']= $value[0];
+		        if (empty($parent_url_parts['extension'])) {
+		            $temp['url'] = 'admin.php?page='. $value[2];  
+		        }else{
+		            $temp['url']= $value[2];
+		        }
+		        $temp['category'] = $value[0];
+		        array_push($full_array, $temp);
+		    }
+		
+		}
+		return $full_array;
+	}
+
+	public static function get_all_searchable_post(){
+		global $wpdb;
 		$all_post_types = array();
-		$available_post_types = array();
+		$post_types = get_post_types('', 'object');
 		foreach ($post_types as $key => $post) {
 		    if ($key == 'attachment' || ($post->show_in_menu == false && $post->public == false)) {
 		        continue;
@@ -80,46 +129,10 @@ class WP_Spotlite_Core{
 		    }
 
 		}
-		$full = array();
-		$join = array();
-		foreach ($menu as $key => $value) {
-		    $home_url_part = pathinfo( $value[2]);
-		    if (!empty($submenu[$value[2]])) {
-		        foreach ($submenu[$value[2]] as $k => $v) {
-		            $temp = array();
-		            $temp['title']= $v[0];
-		            $temp['url']= $v[2];
-		            $temp['parent_url'] = $value[2];
-		            $temp['category'] = $value[0];
-		            $html_url_parts = pathinfo( $temp['url']);
-		            $parent_url_parts = pathinfo( $temp['parent_url']);
-		            if ((empty($html_url_parts['extension']) && empty($parent_url_parts['extension']))) {
-		                $temp['url'] = 'admin.php?page='. $temp['url'];
-		            }elseif (empty($html_url_parts['extension']) && ( !empty($parent_url_parts['extension']) && $parent_url_parts['extension'] == 'php')){
-		                $temp['url'] = $temp['parent_url'].'?page='.$temp['url'];
-		            }elseif (empty($html_url_parts['extension']) && ( !empty($parent_url_parts['extension']))) {
-		                $temp['url'] = $temp['parent_url'].'&'.$temp['url'];
-		            }
-		            array_push($full, $temp);
-		        }
-		    }elseif(!empty($value[0])){
-		        $temp = array();
-		        $temp['title']= $value[0];
-		        if (empty($parent_url_parts['extension'])) {
-		            $temp['url'] = 'admin.php?page='. $value[2];  
-		        }else{
-		            $temp['url']= $value[2];
-		        }
-		        $temp['category'] = $value[0];
-		        array_push($full, $temp);
-		    }
-		
-		}
-		$join = $full;
-		$join = array_merge($join,$all_post_types);
 
-		return $join;
+		return $all_post_types;
 	}
+
 	public static function wp_spotlite_save_settings($data){
 	    if (empty($data['search_include_options'])) {
 	        return false;
