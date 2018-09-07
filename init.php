5b36d0ebea024cfda442b9c6e09ffb50602d5821
rajkuppu <kuppuraj@revmakx.com>
2018-09-07 19:02:22 +0530

    Shortcut key added for input

--- init.php
+++ init.php
@@ -102,7 +102,7 @@ function wp_soptlite_add_toolbar_items($admin_bar){
     global $wp_admin_bar;
 	$form = '<div class="ui search focus" style="background-color: rgba(0, 0, 0, 0);position: relative;">
               <div class="ui left icon input" >
-                <input class="prompt" type="text" placeholder="Search" autofocus style="border-radius: 6px !important;">
+                <input class="prompt" type="text" accesskey="k" placeholder="ctrl + alt + k to search ..." autofocus style="border-radius: 6px !important;">
                 <i class="search icon" style="font-family: FontAwesome;cursor: default;position: absolute;line-height: 1;text-align: center;top: 0;right: 0;margin: 0;height: 100%;width: 2.67142857em;opacity: .5;border-radius: 0 .28571429rem .28571429rem 0;-webkit-transition: opacity .3s ease;transition: opacity .3s ease;background-color: rgba(0, 0, 0, 0);"></i>
               </div>
             </div>
