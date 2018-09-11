08eed4fc2a7f34e4e02e3683c4538bb1536210da
rajkuppu <kuppuraj@revmakx.com>
2018-09-11 19:35:18 +0530

    New Donation button added

--- admin/view/settings.php
+++ admin/view/settings.php
@@ -1,11 +1,19 @@
 <h1 class="wp-spotlight-settings-main-title"><?php _e( 'WP Spotlight Search Settings',  WP_SPOTLIGHT_SEARCH_NAME); ?></h1>
-<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-<input type="hidden" name="cmd" value="_s-xclick">
-<input type="hidden" name="hosted_button_id" value="MWVDKFWB7WMY2">
-<input type="image" src="https://www.paypalobjects.com/en_GB/i/btn/btn_paynow_SM.gif" border="0" name="submit" alt="PayPal – The safer, easier way to pay online!">
-<img alt="" border="0" src="https://www.paypalobjects.com/en_GB/i/scr/pixel.gif" width="1" height="1">
+<form target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+	<input type="hidden" name="cmd" value="_donations">
+	<input type="hidden" name="business" value="rajkuppus@gmail.com">
+	<input type="hidden" name="item_name" value="WP Spotlight Search">
+	<input type="hidden" name="item_number" value="">
+	<input type="hidden" name="currency_code" value="USD">
+	<input type="hidden" name="amount" id="amount_f69666c6f8576f1dc315821705387e01" value="">
+	<input type="hidden" name="no_note" value="1">
+	<input type="hidden" name="no_shipping" value="1">
+	<input type="hidden" name="lc" value="EN_US">
+	<input type="hidden" name="bn" value="WPPlugin_SP">
+	<input type="hidden" name="return" value="">
+	<input type="hidden" name="cancel_return" value="">
+	<input class="wpedon_paypalbuttonimage" type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="Make your payments with PayPal. It is free, secure, effective." style="border: none;"><img alt="" border="0" style="border:none;display:none;" src="https://www.paypal.com/EN_US/i/scr/pixel.gif" width="1" height="1">
 </form>
-
 <div class="wp-spotlight-settings-table" id="wp-spotlight-settings">
 	<form action="admin.php?page=wp_spotlight_menu" method="post">
 		<table class="form-table">
