158068cf0002b591d5fa3623a1a9d6e80f3e7752
rajkuppu <kuppuraj@revmakx.com>
2018-09-20 22:47:56 +0530

    New donation button added

--- admin/view/settings.php
+++ admin/view/settings.php
@@ -1,22 +1,42 @@
 <h1 class="wp-spotlight-settings-main-title"><?php _e( 'WP Spotlight Search Settings',  WP_SPOTLIGHT_SEARCH_NAME); ?></h1>
-<span style="font-weight: 700;line-height: 2">Donating is not just about giving out money it's about making a difference </span> <br>Your donation will make a huge difference in our lives. So please help us to create more awesome plugins like WP Spotlight search by donating us.
-<form target="_blank" action="https://www.paypal.com/cgi-bin/webscr" method="post" style="padding-top: 13px;">
-	<input type="hidden" name="cmd" value="_donations">
-	<input type="hidden" name="business" value="rajkuppus@gmail.com">
-	<input type="hidden" name="item_name" value="WP Spotlight Search">
-	<input type="hidden" name="item_number" value="">
-	<input type="hidden" name="currency_code" value="USD">
-	<input type="hidden" name="amount" id="amount_f69666c6f8576f1dc315821705387e01" value="">
-	<input type="hidden" name="no_note" value="1">
-	<input type="hidden" name="no_shipping" value="1">
-	<input type="hidden" name="lc" value="EN_US">
-	<input type="hidden" name="bn" value="WPPlugin_SP">
-	<input type="hidden" name="return" value="">
-	<input type="hidden" name="cancel_return" value="">
-	<input class="wpedon_paypalbuttonimage" type="image" src="https://www.paypal.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="Make your payments with PayPal. It is free, secure, effective." style="border: none;"><img alt="" border="0" style="border:none;display:none;" src="https://www.paypal.com/EN_US/i/scr/pixel.gif" width="1" height="1">
-</form>
 	<h4>You can control all search functionalities here. You can search following items by ID, Title and Name.</h4>
 <div class="wp-spotlight-settings-table" id="wp-spotlight-settings">
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" class="donation">
+		<table class="form-table">
+			<tbody>
+				<tr>
+					<th>Plugin Support:</th>
+					<td>
+						<div>
+							<label><p><strong>Donating is not just about giving out money it's about making a difference </strong> <br>Your donation will make a huge difference in our lives. So please help us to create more awesome plugins like WP Spotlight search by donating us.</p></label>
+							<br>
+							<input type="hidden" name="cmd" value="_xclick">
+							<input type="hidden" name="business" value="rajkuppus@gmail.com">
+							<input type="hidden" name="item_name" value="WP Spotlight Search (WordPress Plugin)">
+							<input type="hidden" name="buyer_credit_promo_code" value="">
+							<input type="hidden" name="buyer_credit_product_category" value="">
+							<input type="hidden" name="buyer_credit_shipping_method" value="">
+							<input type="hidden" name="buyer_credit_user_address_change" value="">
+							<input type="hidden" name="no_shipping" value="1">
+							<input type="hidden" name="return" value="<?php echo admin_url("admin.php?page=wp_spotlight_menu"); ?>">
+							<input type="hidden" name="no_note" value="1">
+							<input type="hidden" name="currency_code" value="USD">
+							<input type="hidden" name="tax" value="0">
+							<input type="hidden" name="lc" value="US">
+							<input type="hidden" name="bn" value="PP-DonationsBF">
+							<div class="donation-amount">$<input type="number" name="amount" min="16" value="20">
+								<span>
+									<img draggable="false" class="emoji" alt="😀" src="https://s.w.org/images/core/emoji/11/svg/1f600.svg">
+								</span>
+								<input type="submit" class="button-primary" value="Support 💰">
+							</div>
+						</div>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</form>
+	<br><br>
 	<form action="admin.php?page=wp_spotlight_menu" method="post">
 		<table class="form-table">
 			<tr>
