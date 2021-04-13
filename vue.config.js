e346bcc7367e755289ea2789ac615912a6c5f67a
rajkuppu <kuppuraj@live.in>
2021-04-13 21:05:20 +0530

    Config: Config file refactored to support

--- vue.config.js
+++ vue.config.js
@@ -8,26 +8,26 @@ module.exports = {
     liveReload: false,
     sockPort: devPort,
     port: devPort,
-    headers: { "Access-Control-Allow-Origin": "*" }
+    headers: { 'Access-Control-Allow-Origin': '*' },
   },
   publicPath:
-    process.env.NODE_ENV === "production"
-      ? process.env.ASSET_PATH || "/"
+    process.env.NODE_ENV === 'production'
+      ? process.env.ASSET_PATH || '/'
       : `http://localhost:${devPort}/`,
   configureWebpack: {
     output: {
-      filename: `app-`+process.env.VUE_APP_VERSION+`.js`,
-      hotUpdateChunkFilename: "hot/hot-update.js",
-      hotUpdateMainFilename: "hot/hot-update.json"
+      filename: `app-${process.env.VUE_APP_VERSION}.js`,
+      hotUpdateChunkFilename: 'hot/hot-update.js',
+      hotUpdateMainFilename: 'hot/hot-update.json',
     },
     optimization: {
-      splitChunks: false
-    }
+      splitChunks: false,
+    },
   },
   filenameHashing: true,
   css: {
     extract: {
-      filename: `app-`+process.env.VUE_APP_VERSION+`.css`
-    }
-  }
+      filename: `app-${process.env.VUE_APP_VERSION}.css`,
+    },
+  },
 };
