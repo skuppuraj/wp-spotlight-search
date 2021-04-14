86804af3d17496e9ed797e77397205692721088f
rajkuppu <kuppuraj@live.in>
2021-04-14 18:28:06 +0530

    Improve: Text changed

--- admin/view/settings.php
+++ admin/view/settings.php
@@ -1,4 +1,4 @@
-<div><h1 class="wp-spotlight-settings-main-title" style="display: inline-block;"><?php _e( 'WP Spotlight Search Settings',  WP_SPOTLIGHT_SEARCH_NAME); ?></h1><a href="https://wordpress.org/support/plugin/wp-spotlight-search/reviews/#new-post" style="color: #23282d;text-decoration: none;" target="_blank"><img src="<?php echo WP_SPOTLIGHT_SEARCH_URL?>/assets/images/rating.png" style="height: 25px;padding-left: 5px;"> Share your experience
+<div><h1 class="wp-spotlight-settings-main-title" style="display: inline-block;"><?php _e( 'WP Spotlight Search Settings',  WP_SPOTLIGHT_SEARCH_NAME); ?></h1><a href="https://wordpress.org/support/plugin/wp-spotlight-search/reviews/#new-post" style="" target="_blank"><img src="<?php echo WP_SPOTLIGHT_SEARCH_URL?>/assets/images/rating.png" style="height: 25px;padding-left: 5px;">Share your experience
 </a>
 </div>
 	<h4>You can control all search functionalities here. You can search following items by ID, Title and Name.</h4>
